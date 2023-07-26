e86560f1d07ef410db0f70f0e60bba7b8b20ef26
CHESS CHAPaaS <chess_chapaas@lnx15.classe.cornell.edu>
2023-07-26 12:18:27 -0400

    CHAPBook commit update for suchismitasarker on Wed Jul 26 04:18:26 PM UTC 2023

---
+++ suchismitasarker/userprocessor.py
@@ -0,0 +1,33 @@
+from CHAP.pipeline import PipelineItem
+
+class UserProcessor(PipelineItem):
+    """Generic user processor.
+    """
+
+    def process(self, data):
+        """Extract the contents of the input data, add a string to it,
+        and return the amended value.
+
+        :param data: input data
+        :return: processed data
+        """
+        # If needed, extract data from a returned value of Reader.read
+        #if isinstance(data, list):
+        #    if all(isinstance(d, dict) for d in data):
+        #        data = data[0]['data']
+        #if data is None:
+        #    return []
+
+        # user code
+
+        ### Welcome to CHAP notebook, user suchismitasarker
+
+        # CHAP imports
+        from CHAP.pipeline import PipelineItem
+        # define pipeline object
+        pipeline = PipelineItem() 
+        print('hello Suchi')
+        print('CHAPBook is awesome!')
+
+        # and we return data back to pipeline
+        return data
